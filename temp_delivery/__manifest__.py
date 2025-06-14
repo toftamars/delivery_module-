736e249e537868dacaa4477ce0e281c114c14649
toftamars <GitHub E-posta Adresiniz>
2025-06-15 01:02:39 +0300

    Modül yapısı düzenlendi

--- temp_delivery/__manifest__.py
+++ temp_delivery/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Teslimat Modülü',
     'version': '16.0.1.0.0',
-    'category': 'Inventory/Delivery',
+    'category': 'Inventory',
     'summary': 'Transfer belgelerinden otomatik teslimat oluşturma ve yönetim',
     'description': """
         Teslimat Modülü:
