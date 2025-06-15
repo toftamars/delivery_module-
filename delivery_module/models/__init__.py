533ed7c2f84fae6ee1715f5d34c284737f5d16ec
toftamars <GitHub E-posta Adresiniz>
2025-06-15 18:28:52 +0300

    Import sırası basitleştirildi

--- delivery_module/models/__init__.py
+++ delivery_module/models/__init__.py
@@ -1,7 +1,2 @@
-from . import res_city
-from . import res_city_district
-from . import res_partner
-from . import delivery_day
-from . import delivery_planning
 from . import delivery_document
 from . import stock_picking 
