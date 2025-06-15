60fa3d6146403d8f81b0b5dda11beff87e82a482
toftamars <GitHub E-posta Adresiniz>
2025-06-15 13:55:33 +0300

    Teslimat günü modeli güncellendi

--- delivery_module/models/delivery_day.py
+++ delivery_module/models/delivery_day.py
@@ -3,9 +3,11 @@ from odoo import models, fields, api
 class DeliveryDay(models.Model):
     _name = 'delivery.day'
     _description = 'Teslimat Günü'
+    _order = 'sequence, name'
 
     name = fields.Char(string='Gün Adı', required=True)
     code = fields.Char(string='Gün Kodu', required=True)
+    sequence = fields.Integer(string='Sıra', default=10)
     active = fields.Boolean(default=True)
     company_id = fields.Many2one('res.company', string='Şirket', required=True, default=lambda self: self.env.company)
 
