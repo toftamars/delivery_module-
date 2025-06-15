10d51a703c8c8bba314b94d97fa18be9349e564e
toftamars <GitHub E-posta Adresiniz>
2025-06-15 14:00:59 +0300

    Müşteri modelindeki teslimat günleri alanı düzenlendi

--- delivery_module/models/res_partner.py
+++ delivery_module/models/res_partner.py
@@ -5,4 +5,10 @@ class ResPartner(models.Model):
 
     is_driver = fields.Boolean(string='Sürücü mü?')
     district_id = fields.Many2one('res.city.district', string='İlçe')
-    delivery_day_ids = fields.Many2many('delivery.day', string='Teslimat Günleri') 
+    delivery_day_ids = fields.Many2many(
+        'delivery.day',
+        'res_partner_delivery_day_rel',
+        'partner_id',
+        'day_id',
+        string='Teslimat Günleri'
+    ) 
