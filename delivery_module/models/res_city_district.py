fe04214f7ccbe446dcda156b3fc7a676dd2d3838
toftamars <GitHub E-posta Adresiniz>
2025-06-15 14:02:02 +0300

    İlçe modelindeki teslimat günleri alanı düzenlendi

--- delivery_module/models/res_city_district.py
+++ delivery_module/models/res_city_district.py
@@ -6,6 +6,12 @@ class ResCityDistrict(models.Model):
 
     name = fields.Char(string='İlçe Adı', required=True)
     city_id = fields.Many2one('res.city', string='İl', required=True)
-    delivery_day_ids = fields.Many2many('delivery.day', string='Teslimat Günleri')
+    delivery_day_ids = fields.Many2many(
+        'delivery.day',
+        'res_city_district_delivery_day_rel',
+        'district_id',
+        'day_id',
+        string='Teslimat Günleri'
+    )
     active = fields.Boolean(default=True)
     company_id = fields.Many2one('res.company', string='Şirket', required=True, default=lambda self: self.env.company) 
